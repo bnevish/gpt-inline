18729fa4b36c486b597489a1647409223d828dad
bnevish <nevish.b96@gmail.com>
2024-01-26 09:48:17 +0530

    test

--- js/starter.js
+++ js/starter.js
@@ -1,1 +1,19 @@
-console.log("Loading starter.js");
+// highlightFetcher.js
+
+document.addEventListener("mouseup", function() {
+    var highlightedText = getHighlightedText();
+    if (highlightedText) {
+        console.log("Highlighted Text: " + highlightedText);
+        // You can do whatever you want with the highlighted text here
+    }
+});
+
+function getHighlightedText() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
