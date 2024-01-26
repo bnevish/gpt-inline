62d5c5fc753bb6f713d43585d645a6960598a21e
bnevish <nevish.b96@gmail.com>
2024-01-26 10:03:48 +0530

    final

--- js/starter.js
+++ js/starter.js
@@ -1,19 +1,23 @@
-// highlightFetcher.js
-
 document.addEventListener("mouseup", function() {
     var highlightedText = getHighlightedText();
     if (highlightedText) {
         console.log("Highlighted Text: " + highlightedText);
         // You can do whatever you want with the highlighted text here
+    } else {
+        console.log("No text is highlighted.");
     }
 });
 
 function getHighlightedText() {
     var text = "";
     if (window.getSelection) {
-        text = window.getSelection().toString();
+        var selection = window.getSelection();
+        if (selection && selection.toString()) {
+            text = selection.toString();
+        }
     } else if (document.selection && document.selection.type != "Control") {
         text = document.selection.createRange().text;
     }
     return text;
 }
+
